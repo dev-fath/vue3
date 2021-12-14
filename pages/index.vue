3e1439f86c0c4c67ec10737e601e0ab2c7f7ffb2
Fathory <ggje33@gmail.com>
2021-12-14 13:33:14 +0900

    [app] nuxt vue app practice

---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="counter">
+    Counter: {{ counter }}
+    {{isZero}}
+  </div>
+  <button @click="countUp"> counter up </button>
+  <button @click="countDown"> counter up </button>
+</template>
+
+<script language="ts">
+
+const countUp = () => {
+  this.counter++;
+}
+const countDown = () => {
+  this.counter--;
+}
+const index = {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  beforeCreate() {
+    console.log('bc')
+  },
+  created() {
+    console.log('c')
+  },
+  beforeMount() {
+    console.log('bm')
+  },
+  mounted() {
+    console.log('m')
+  },
+  beforeUpdate() {
+    console.log('bu')
+  },
+  updated() {
+    console.log('u')
+  },
+  beforeUnmount() {
+    console.log('bum')
+  },
+  unmounted() {
+    console.log('um')
+  },
+  methods: {
+    countUp() {  this.counter++; },
+    countDown() {  this.counter--; },
+  },
+  computed: {
+    isZero() {
+      return this.counter === 0;
+    }
+  }
+}
+export default {
+  name: 'counter',
+  ...index,
+}
+</script>
+
+<style scoped>
+
+</style>
